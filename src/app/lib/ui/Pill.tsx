1266bf6c3de508e260ee5d122cae390b0fa69b5c
ai-is-awesome <irock0093@gmail.com>
2024-06-12 06:53:21 +0530

    changed bg of job list

--- src/app/lib/ui/Pill.tsx
+++ src/app/lib/ui/Pill.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export const Pill = ({ children, className, ...restOfProps }) => {
   const classes = classNames(
-    "text-gray-800 hover:bg-gray-200 bg-white px-4 py-1 rounded-full text-sm",
+    "hover:bg-gray-200 bg-white px-3 py-1 rounded-full text-sm ",
     className
   );
   return <button className={cn(classes)}>{children}</button>;
