a4d201893df2dfac02af3e26699af4e0bd2c8157
ai-is-awesome <irock0093@gmail.com>
2024-07-16 16:35:32 +0530

    fixed horizontal scroll

--- src/app/lib/ui/components/jobCardList.tsx
+++ src/app/lib/ui/components/jobCardList.tsx
@@ -7,7 +7,7 @@ type JobCardListInterface = {
 const JobCardList = ({ children }: JobCardListInterface) => {
   // Grid of 2 columns
   return (
-    <div className="lg:grid-cols-2 grid-cols-1 grid gap-x-[2rem] gap-y-[1rem] p-4 bg-gray-50">
+    <div className="lg:grid-cols-2 grid-cols-1 grid gap-x-[2rem] gap-y-[1rem] p-0 lg:p-4 bg-gray-50">
       {children}
     </div>
   );
